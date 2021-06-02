7c1986d5baaf41c268e7521ad6de023f6d74103a
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-06-02 14:22:56 -0700

    added notes alike to gp notes from josh jacobson

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,6 +2,8 @@ import useLocalStorage from './useLocalStorage';
 
  const useDarkMode = (key, value) => {
     const [darkMode, setDarkMode] = useLocalStorage(key, value);
+    
+// return value should be something like [darkMode, setDarkMode]
 return [darkMode, setDarkMode]
 };
 
