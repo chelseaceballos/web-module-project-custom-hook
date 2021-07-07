00983bf1fbcf6f5d4c618be6b047e580f6aa216d
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-07-07 11:04:15 -0700

    MVP

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,7 @@
+import {useLocalStorage} from './useLocalStorage';
+// this isn't doing any actual logic on it's own, but if it were it would be checking the OS user preferences and change based on if the user has light or dark mode set.
 
-
-const useDarkMode = () => {
-    
+export const useDarkMode = (initialValue) => {
+    const [value, setValue] = useLocalStorage('darkmode', initialValue);
+    return [value, setValue]
 }
